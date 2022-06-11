8bc3e0b715ff38c270fa3eb79e188449d1ed8258
sdott3 <sdotthomas347@gmail.com>
2022-06-11 09:47:52 -0400

    html page finished and styled with bootstrap

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var cityinput = document.getElementById("#city-input");
+var search = document.getElementById("#add");
+var city = document.getElementById("#city");
+
+
+apik = "5b7723185b34f31fa8ce7661dee07b75"
