c4a533d37d6f3c3c57b3ad76b65020d1ca51fff9
sdott3 <sdotthomas347@gmail.com>
2022-06-12 21:40:12 -0400

    updated html page, declared variables in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,8 @@
-var cityinput = document.getElementById("#city-input");
-var search = document.getElementById("#add");
-var currentCity = document.getElementById("#current-city");
-var currentTemp = document.getElementsByClassName("#temp");
-var currentHumidity = document.getElementsByClassName("#humidity");
-var currentWspeed = document.getElementsByClassName("#wind-speed");
-var currentUVindex = document.getElementsByClassName("#uv-index");
+var search = document.getElementById("search");
+var searchButton = document.getElementById("searchButton");
+var currentWeather = document.getElementById("current-weather");
+var fiveDayForecast = document.getElementById("fiveDayForecast");
+var fiveDayForecastHeader = document.getElementById("fiveDayForecastHeader");
 
 /* Custom api key */
 var apiKey = "5b7723185b34f31fa8ce7661dee07b75";
