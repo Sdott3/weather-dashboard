4b1dbb4402916a0ca275e71d049e2970fffea7b7
sdott3 <sdotthomas347@gmail.com>
2022-08-11 14:21:35 -0400

    updated search container and 5 day forcast result display in index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,16 @@ var fiveDayForecast = document.getElementById("fiveDayForecast");
 var fiveDayForecastHeader = document.getElementById("fiveDayForecastHeader");
 
 /* Custom api key */
-var apiKey = "5b7723185b34f31fa8ce7661dee07b75";
+var apiKey = "87f3c974028e429a51503e5e3d883b50";
+
+/* function to search city */
+function searchCity() {
+    var cityname = document.getElementById("city-input").value;
+    var url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityname}&appid=${apiKey}`;
+
+}
+
+/* function for 5-Day forecast */
+function fiveDayForecast() {
+
+}
